a7cb9f39687880d95a0338163494acb7b27c088f
my <my@163.com>
2020-01-15 10:40:38 +0800

    1111111111

--- Admin.java
+++ Admin.java
@@ -1,12 +1,5 @@
 public class Hello{
 	public Hello(){
-		System.out.println("我是MASTER分支中的Hello类");
-System.out.println("我是MASTER分支中的Hello类");
-System.out.println("我是MASTER分支中的Hello类");
-System.out.println("我是MASTER分支中的Hello类");
-		System.out.println("我是DEV分支中的Hello类");
-System.out.println("我是DEV分支中的Hello类");
-System.out.println("我是DEV分支中的Hello类");
-System.out.println("我是DEV分支中的Hello类");
+		
 	}
 }
